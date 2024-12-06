fa60759d44334d69b04273058ac1d1c811ffa85f
miguel.cortecero.tor <miguel.cortecero.torres@tecsisa.com>
2024-12-06 15:15:51 +0100

    Initial commit: add project scaffolding

---
+++ src/App.stories.tsx
@@ -0,0 +1,27 @@
+import { expect, userEvent, within } from '@storybook/test';
+import type { Meta, StoryObj } from '@storybook/react';
+import App from './App';
+
+const meta = {
+    title: 'Demo/App',
+    component: App,
+    // tags: ["docsPage"],
+    parameters: {
+        layout: 'centered',
+    },
+} satisfies Meta<typeof App>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
+
+export const Interactive: Story = {};
+Interactive.play = async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const incrementButton = canvas.getByRole('button');
+
+    await userEvent.click(incrementButton);
+
+    await expect(incrementButton.textContent).toContain('count is 1');
+};
